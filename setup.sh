5f0d9b50cc1fd70e5b3b2f7039e45acd7e04bce3
APT-GET47 <57804710+MALW4R3@users.noreply.github.com>
2020-06-12 00:49:38 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+pkg install figlet -y
+pkg install toilet -y
+pkg install python -y
+pkg install python2 -y
+pkg install ruby -y
+gem install lolcat
+pkg install hydra -y
+pkg install nmap -y
+pkg install openssh -y
+pip2 install sqlmap
+pip2 install requests
+git clone https://github.com/hahwul/XSpear
+cd XSpear
+gem install XSpear-1.4.1.gem
+cd ..
+mv nebula /data/data/com.termux/files/usr/bin
+cd
+cd ..
+cd usr
+cd bin
+chmod +x nebula
+cd
+cd NEBULA
+clear
+cat finished.txt | lolcat
+
